e34ebe0037ddb2eca62141208cb04ba6ba2bf194
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-23 14:46:51 +0000

    Challange 12 raise error message when releasing bikes if none available

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,22 @@
 require_relative 'bike'
+
 class DockingStation
   attr_reader :docked_bike
-  
-  def release_bike
+
+  def request_bike
     Bike.new
   end
-  
+
+  def release_bike
+    if self.docked_bike
+      @docked_bike
+    else
+      fail "Sorry, no bikes."
+    end
+  end
+
   def dock(bike)
     @docked_bike = bike
   end
-  
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,25 @@
 require "docking_station.rb"
 describe DockingStation do
-  it {is_expected.to respond_to(:dock).with(1).argument}
 
-  it "should return relased bike when called" do
+  it "should create a new bike when called" do
+    expect(subject.request_bike).to be_a_kind_of(Bike)
+  end
+
+  it "should return relased bike when called and there is a bike" do
+    subject.dock(Bike.new)
     expect(subject.release_bike).to be_a_kind_of(Bike)
   end
+
+  it "raise an error when #release_bike is called but there are no bikes" do
+    expect {subject.release_bike}.to raise_error("Sorry, no bikes.")
+  end
+
+  it {is_expected.to respond_to(:dock).with(1).argument}
 
   it "should return bike when called" do
     expect(subject.dock(@bike)).to eq @bike
   end
 
   it { is_expected.to respond_to(:docked_bike)}
+
 end
