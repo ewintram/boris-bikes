664132d66306d96a8b76e61b4937b8c743ce2f20
umairb1 <umairbashir95@hotmail.com>
2017-11-23 16:52:02 +0000

    completed step 15 created private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -12,14 +12,24 @@ class DockingStation
   end
 
   def release_bike
-    fail "Sorry, no bikes." if @docked_bikes.empty?
+    fail "Sorry, no bikes." if empty?
     @docked_bikes.pop
   end
 
   def dock(bike)
-    fail "Sorry, no more capacity." if @docked_bikes.length >= 20
+    fail "Sorry, no more capacity." if full?
     @docked_bikes.push(bike)
     @docked_bikes.last
   end
 
+  private
+
+  def full?
+    @docked_bikes.length == 20
+  end
+  
+  def empty?
+    @docked_bikes.length == 0
+  end
 end
+
